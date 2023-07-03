9671e4bd853419bae272a1a24ecd70a275254649
Sebastian <bastianonofrei@gmail.com>
2023-07-03 16:42:10 +0300

    created footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/organisms/about/About"
+import Footer from "./components/organisms/footer/Footer"
 import Hero from "./components/organisms/hero/Hero"
 import Newsletter from "./components/organisms/newsletter/Newsletter"
 import OurMission from "./components/organisms/our-mission/OurMission"
---
+++ src/components/organisms/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+		<footer>
+			<p>©Copyright 2023 Château d'Excellence. All rights reserved.</p>
+		</footer>
+	);
+}
+
+export default Footer
