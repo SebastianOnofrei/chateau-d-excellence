8ab94912a80ea519a12c0af0fa95b417d7a3f4cc
Sebastian <bastianonofrei@gmail.com>
2023-07-12 22:21:07 +0300

    improved mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <>
      <Hero/>
-     {/* <About/>
+     <About/>
      <OurMission/>
      <Portfolio/>
-     <Newsletter/>
+     {/* <Newsletter/>
      <Footer/> */}
     </>
   )
